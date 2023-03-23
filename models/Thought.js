c1b362e153b1fd4ae9db89d2b59c35e4bbb284f2
waheec19 <waheechowdhury80@gmail.com>
2023-03-23 17:53:24 -0500

    new change

--- models/Thought.js
+++ models/Thought.js
@@ -10,13 +10,7 @@ const thoughtSchema = new Schema(
       minLength: 1,
       maxLength: 280,
     },
-    createdAt: {
-     
-      type: Date,
-      default: Date.now,
-  
-      get: (date) => moment(date).format("MMM DD, YYYY [at] hh:mm a"),
-    },
+
     username: {
       type: String,
       required: true,
